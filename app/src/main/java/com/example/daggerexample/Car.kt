6cf0e22b3ca19c6c59e566b5ad975073ab62da06
PabloSaba <psabaliauskas@teco.com.ar>
2019-07-19 12:26:57 -0300

    init

---
+++ app/src/main/java/com/example/daggerexample/Car.kt
@@ -0,0 +1,12 @@
+package com.example.daggerexample
+
+import android.util.Log
+
+class Car {
+
+
+    fun driver(){
+        Log.d("Car", "runnnnnnn")
+
+    }
+}
---
+++ app/src/main/java/com/example/daggerexample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.daggerexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var car : Car
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        car = Car()
+
+        car.driver()
+
+    }
+}
